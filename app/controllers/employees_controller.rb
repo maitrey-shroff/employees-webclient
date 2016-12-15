8a5f13961a743c86044e375aff5094761004394b
maitrey-shroff <maitrey89@me.com>
2016-12-14 19:18:07 -0800

    consuming more APIs to do edit and updates

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,7 +1,11 @@
 class EmployeesController < ApplicationController
 
+  def index
+    @employees = Unirest.get("#{ENV["API_URL"]}/employees").body
+  end
+
   def show
-    @employee = Unirest.get("localhost:3000/api/v2/employees/#{params[:id]}.json").body
+    @employee = Unirest.get("#{ENV["API_URL"]}/employees/#{params[:id]}.json").body
   end
 
   def new
@@ -13,22 +17,23 @@ class EmployeesController < ApplicationController
     input_first_name = params[:first_name]
     input_last_name = params[:last_name]
 
-    Unirest.post("localhost:3000/api/v1/create", parameters: {first_name: input_first_name, last_name: input_last_name}, 
+    Unirest.post("#{ENV["API_URL"]}/create", parameters: {first_name: input_first_name, last_name: input_last_name}, 
       headers: {"Accept" => "application/json"})
 
-    redirect_to "/employees/7"
+    redirect_to "/employees"
   end
 
   def edit
-
+    @employee = Unirest.get("#{ENV["API_URL"]}/employees/#{params[:id]}.json").body
   end
 
   def update
-
+    @employee = Unirest.patch("#{ENV["API_URL"]}/update/#{params[:id]}", parameters: {first_name: params[:first_name], last_name: params[:last_name]}, 
+      headers: {"Accept" => "application/json"}).body
   end
 
   def destroy
-    @employee = Unirest.delete("localhost:3000/api/v1/delete/#{params[:id]}")
+    @employee = Unirest.delete("#{ENV["API_URL"]}/delete/#{params[:id]}")
   end
 
 
